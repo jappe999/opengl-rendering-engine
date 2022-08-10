80569541f97f00feaf8e0292e1174d1563e93586
Jasper van der Linden <jappe_1999@outlook.com>
2022-08-10 15:18:37 +0200

    wip

---
+++ rock/ui/renderer/render_graph.cpp
@@ -0,0 +1,14 @@
+#include "./render_graph.hpp"
+#include <ore/engine.hpp>
+
+namespace Rock
+{
+  void RenderGraph::onImGUI()
+  {
+    if(ImGui::Begin("Render graph")){
+
+    // TODO
+
+    ImGui::End();}
+  }
+} // namespace Rock
